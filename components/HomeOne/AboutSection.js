9c9d14d2d26b2bf4db86d2973da45c639fd471cb
capitalhealthcaree <premierpatientappointment@gmail.com>
2023-07-31 10:12:38 +0500

    add style of about section

--- components/HomeOne/AboutSection.js
+++ components/HomeOne/AboutSection.js
@@ -35,38 +35,105 @@ const AboutSection = () => {
 
                 <ul>
                   <div className="row">
-                    <div className="col-sm-12 col-lg-6">
+                    <div className="d-none d-sm-block col-xl-6">
                       <li>
                         <i className="icofont-check-circled"></i>
-                        Charity For Social Justice
+                        Charity For{" "}
+                        <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                          S
+                        </span>
+                        ocial Justice
                       </li>
                       <li>
                         <i className="icofont-check-circled"></i>
-                        Charity For Advocacy
+                        Charity For{" "}
+                        <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                          A
+                        </span>
+                        dvocacy
                       </li>
                       <li>
                         <i className="icofont-check-circled"></i>
-                        Charity For Education
+                        Charity For{" "}
+                        <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                          E
+                        </span>
+                        ducation
                       </li>
                     </div>
 
-                    <div className="col-sm-12 col-lg-6 mt-2">
+                    <div className="d-none d-sm-block col-xl-6">
                       <li>
                         <i className="icofont-check-circled"></i>
-                        Charity For Healthcare
+                        Charity For{" "}
+                        <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                          H
+                        </span>
+                        ealthcare
                       </li>{" "}
                       <li>
                         <i className="icofont-check-circled"></i>
-                        Charity For Poverty
-                      </li>{" "}
+                        Charity For{" "}
+                        <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                          P
+                        </span>
+                        overty
+                      </li>
                       <li>
                         <i className="icofont-check-circled"></i>
                         24/7 Online Support
-                      </li>{" "}
+                      </li>
                     </div>
                   </div>
                 </ul>
-
+                <ul>
+                  <div className="row d-xl-none">
+                    <li>
+                      <i className="icofont-check-circled"></i>
+                      Charity For{" "}
+                      <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                        S
+                      </span>
+                      ocial Justice
+                    </li>
+                    <li>
+                      <i className="icofont-check-circled"></i>
+                      Charity For{" "}
+                      <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                        H
+                      </span>
+                      ealthcare
+                    </li>{" "}
+                    <li>
+                      <i className="icofont-check-circled"></i>
+                      Charity For{" "}
+                      <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                        A
+                      </span>
+                      dvocacy
+                    </li>
+                    <li>
+                      <i className="icofont-check-circled"></i>
+                      Charity For{" "}
+                      <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                        P
+                      </span>
+                      overty
+                    </li>
+                    <li>
+                      <i className="icofont-check-circled"></i>
+                      Charity For{" "}
+                      <span style={{ color: "#0046c0", fontWeight: "bold" }}>
+                        E
+                      </span>
+                      ducation
+                    </li>
+                    <li>
+                      <i className="icofont-check-circled"></i>
+                      24/7 Online Support
+                    </li>
+                  </div>
+                </ul>
                 <Link href="/donation">DONATE</Link>
               </div>
             </div>
