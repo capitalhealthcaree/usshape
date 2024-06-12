8ace2f20c4030b72ea6f746f108ec1c7d14d5335
capitalhealthcaree <premierpatientappointment@gmail.com>
2024-06-13 01:42:20 +0500

    update paypal client id

--- pages/_document.js
+++ pages/_document.js
@@ -1,26 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
-    const { locale } = this.props.__NEXT_DATA__;
-    const dir = locale === "ar" ? "rtl" : "ltr";
-    return (
-      <Html dir={dir} lang={locale}>
-        <Head>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
-            rel="stylesheet"
-          />
-          <link rel="icon" type="image/png" href="/images/favicon.png"></link>
-          <script src="https://www.paypal.com/sdk/js?client-id=AXVHach6gezXNGcCaAXZOZm8Db9N5VJt8ga9nW1pJ9Iokx7QMbbhdeIsixtpDaBC0WP4Vy2E2Nz7_Pfl&disable-funding=venmo,paylater"></script>
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		const { locale } = this.props.__NEXT_DATA__;
+		const dir = locale === "ar" ? "rtl" : "ltr";
+		return (
+			<Html dir={dir} lang={locale}>
+				<Head>
+					<link
+						href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+						rel="stylesheet"
+					/>
+					<link rel="icon" type="image/png" href="/images/favicon.png"></link>
+					<script src="https://www.paypal.com/sdk/js?client-id=AVCLROfYK2F6xVcWMQ8sECn1bI6Ev2HifAgw8bDSDusNoswSmt5cfJm4tZx_e7HXeAvn67YJoyg-XBD6&disable-funding=venmo,paylater"></script>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
 export default MyDocument;
+//Dr Paypal
+//<script src="https://www.paypal.com/sdk/js?client-id=AXVHach6gezXNGcCaAXZOZm8Db9N5VJt8ga9nW1pJ9Iokx7QMbbhdeIsixtpDaBC0WP4Vy2E2Nz7_Pfl&disable-funding=venmo,paylater"></script>
+//MAhu Dubai Paypal
+//AVCLROfYK2F6xVcWMQ8sECn1bI6Ev2HifAgw8bDSDusNoswSmt5cfJm4tZx_e7HXeAvn67YJoyg-XBD6
