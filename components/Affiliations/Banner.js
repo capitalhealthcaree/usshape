1a051a74c401a13ce3e9dce40783d53a47850577
capitalhealthcaree <premierpatientappointment@gmail.com>
2026-06-20 03:28:07 +0500

    dsdsf

--- components/Affiliations/Banner.js
+++ components/Affiliations/Banner.js
@@ -6987,7 +6987,7 @@ export default function Hero() {
         <div className="containers">
           <div className="m-image-wrap">
             <img
-              src="/images/7.png"
+              src="/images/11.jpeg"
               alt="Premier Pain Centers x Sam Houston State University Partnership"
               className="m-image"
             />
